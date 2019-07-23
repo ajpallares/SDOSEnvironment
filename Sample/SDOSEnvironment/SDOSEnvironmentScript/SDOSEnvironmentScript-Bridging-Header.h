87aab23a3862753b7378aedc20b7c1ea10955432
Rafael Fernández <rafael89.fernandez@gmail.com>
2019-07-23 12:31:09 +0200

    Install dependency

---
+++ Sample/SDOSEnvironment/SDOSEnvironmentScript/SDOSEnvironmentScript-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SDOSEnvironmentScript:SDOSEnvironmentScript-Bridging-Header.h
+//  SDOSEnvironmentExample
+//
+//  Created by Rafael Fernandez Alvarez on 23/07/2019.
+//  Copyright © 2019 SDOS. All rights reserved.
+//
+
+//#import "RNCryptor.h"SDOSEnvironmentScript-Bridging-Header.h
